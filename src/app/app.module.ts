ef7fa4d4dc04c5c946694a33856c1437784cc2a2
Celeste991 <celeste.briceno92@student.qcc.cuny.edu>
2020-11-19 11:36:32 -0500

    Everything_works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartService } from './cart.service';
-import { CartComponent } from './cart/cart.component';
 import { MyResumeComponent } from './my-resume/my-resume.component';
-import { ShippingComponent } from './shipping/shipping.component';
 import { ResumeEducationComponent } from './resume-education/resume-education.component';
 import { ResumeFooterComponent } from './resume-footer/resume-footer.component';
 import { ResumeProfileComponent } from './resume-profile/resume-profile.component';
 import { ResumeWorkExperienceComponent } from './resume-work-experience/resume-work-experience.component';
+import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'products/:productId', component: 
+      ProductDetailsComponent },
       { path: 'resume', component: MyResumeComponent },
       { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent },
+       { path: 'shipping', component: ShippingComponent },
 
-      
     ])
   ],
   declarations: [
@@ -37,13 +39,13 @@ import { ResumeWorkExperienceComponent } from './resume-work-experience/resume-w
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent,
     MyResumeComponent,
-    ShippingComponent,
     ResumeEducationComponent,
     ResumeFooterComponent,
     ResumeProfileComponent,
-    ResumeWorkExperienceComponent
+    ResumeWorkExperienceComponent,
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
